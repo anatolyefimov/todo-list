69bafb3a9fc7bd1531fcb80f4854ffb08cfe959a
anatolyefimov <efimo.tolik@gmail.com>
2019-09-05 00:41:27 +0300

    init frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			Долого не смотри может хуй в рот залететь!!!
+		</div>
+	);
+}
+
+export default App;
